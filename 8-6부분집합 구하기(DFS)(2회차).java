fc61ad89405722995f16c1687ee3265355b0fc8d
xodnrok <103110967+xodnrok@users.noreply.github.com>
2026-01-06 16:38:24 +0900

    Update and rename 8-6부분집합 구하기(DFS) to 8-6부분집합 구하기(DFS)(2회차).java
    DFS를 보고 노드객체를 항상 만들어서 사용하는 줄 알았는데 이번엔 그게 아니였다. 저번 문제는 노드의 lt,rt가 다른 노드의 주소를 가리켰지만 이번엔 그저 숫자를 가르키는 것으로 문제를 풀었다. 또한 왼쪽 오른쪽 기준이 자신을 집합에 사용할 경우 왼쪽 사용하지 않을경우 오른쪽으로 생각하면서 풀어야 한다.
    
    코드에서는 DFS(L + 1) 이 똑같은게 2번나왔지만 첫번째는 왼쪽 두번째는 오른쪽이라 생각하고 풀어야 한다.
    
    메인함수, 메서드에서 서로 공유하는 변수를 써야 하므로 멤버변수로 변수로 선언을했다.  자신의 숫자를 쓴다는 것을 표시하기 위해 배열에 값을 넣고 사용할 경우 1 사용하지 않을 경우 0 으로 표시했다.
    또한 인덱스의 값을 값으로 쓰기 때문에 배열의 크기를 n+1로 표시했다.
    
    자신이 원한 숫자를 탐색하다 넘어가는경우 해당 배열에 있는 1로표시된 애들을 전부다 출력한다.
    
    2회차 문제풀이
    
    처음에 문제보고 풀지도 못했고 부분집합도 생각못했다.
    정답을 보고 직접 손으로 스택을 따라가면서 정답과 똑같이 나오게 난후 문제를 다시 풀었다.
    
    만약 또 다시 기억이 안나서 이걸본다면
    
    부분집합은 예를 들어 3이라고 했다면 1부터 3까지 숫자를 사용하며 그 숫자를 사용해서 3개의 숫자를 만들던가 혹은 사용하지 않아서 2개의 숫자 ,1개의 숫자를 만들수 있다.
    
    그리고 사용 할 지 말지는 배열을 사용해서 풀어야 한다.

--- 8-6부분집합 구하기(DFS)(2회차).java
+++ 8-6부분집합 구하기(DFS)(2회차).java
@@ -51,4 +51,56 @@ public class Test {
     }
 
 }
+
+2회차 문제 풀이
+
+import java.util.*;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+class Main {
+
+    static int[] arr;
+    static int n;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        n = input.nextInt(); // 사용자 입력 값
+
+        arr = new int[n + 1]; //인덱스의 값을 숫자로 사용하기 때문에 n+1 했음
+
+        DFS(1);
+
+    }
+
+    private static void DFS(int i) {
+
+        if (i > n) {
+//            String answer = ""; //정답 출력 변수  성능최적화를 위해 문자가 계속 바뀌므로 스트링빌더 사용하는게 좋다.
+            StringBuilder sb = new StringBuilder();
+            for (int j = 1; j < arr.length; j++) {
+                if (arr[j] == 1) {
+//                    answer += j + " ";
+                    sb.append(j + " ");
+                }
+            }
+            if (!sb.isEmpty()) {
+                System.out.println(sb);
+            }
+
+        } else {
+            arr[i] = 1; //이 숫자를 넣기 때문에 1을 줌
+            DFS(i + 1); //다음 숫자도 넣을지 말지 체크
+
+            arr[i] = 0; //이 숫자를 빼기 때문에 0을줌
+            DFS(i + 1); //해당 숫자를 빼고 나서 다음 숫자 넣을지 말지 체크
+
+        }
+    }
+
+}
+
+
                      
