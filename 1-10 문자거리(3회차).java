b09c350c6b53a48200de98bdff47dbfe4312c594
xodnrok <103110967+xodnrok@users.noreply.github.com>
2026-07-24 15:27:28 +0900

    Update and rename 1-10 문자거리(3회차).java to 1-10 문자거리(3회차).java
    거리 변수를 사용해서 입력받은 문자를 앞에서부터 한번 반복해서 돌고 뒤로부터 해서 한 번 반복하는데 이때 앞에서 한 반복과 뒤로 하는 반복문에서 거리 값을 비교해서 더 작은 거리 값을 answer 배열에 저장한다.
    
    거리의 처음 변수를 1000으로 하는 이유는 앞에 타겟문자가 멀리 떨어져 있을수도 있기 때문
    
    2회차 풀이
    
    문자열에서 문자로 지정한 문자마다 반복문으로 찾아서 찾았을때 또 한번 반복문을 돌려서 오른쪽방향으로한번 왼쪽방향으로 한번 왼쪽방향으로 할때는 오른쪽방향과 비교해서 최솟값을 정답배열에 저장하도록 풀었다. 1회차때의 알고리즘이 안떠올라서 풀긴풀었지만 1회차 풀이가 더 좋은 풀이이다.
    
    tow pass 알고리즘 사용해서 풀었을때 p++ 을 하고 answer 배열에 값을 넣었어야 했는데 answer[i] = p++을 해버려서 증가된 p의 값을 넣었어야 했는데 p++은 후위 연산이라 배열에 증가되기 전 p의값이 들어가고 그 이후 p값이 증가해서 에러가 났다. 그로직 빼고는 정답이였다.
    
    투 패스(Two-Pass) 알고리즘은 어떤 요소의 최종 결과값이 그 요소의 '왼쪽' 정보와 '오른쪽' 정보 모두에 의존할 때 사용하는 것이 가장 좋습니다.
    
    
    3회차 풀이
    
    처음 보고나서 카운트 변수로 왼쪽으로 한번 오른쪽으로 한번 이때 최솟값을 저장한다는거 까지 생각해서 풀었는데 우리가 찾고자 하는 문자를 만나기 전까지는 쓰래기값을 저장하기 위해서 카운트의 초기값을 엄청 큰 숫자로 했어야 하는데 1로 시작한게 문제였고 정답을 배열에 저장했어야 하는데 리스트에 저장한게 문제였다.
    
    이후 count를 초기값을 1000 으로 시작하고 배열로 바꾸니 정상적으로 답이 나왔다.
    
    즉 우리가 찾고자 하는 문자를 만나기 전까지는 쓰래기 데이터 이므로 값을 엄청 크게 줘야 나중에 오른쪽에서 탐색할때 올바르게 최솟값을 찾을 수 있다.

--- 1-10 문자거리(3회차).java
+++ 1-10 문자거리(3회차).java
@@ -150,6 +150,126 @@ class Main {
 }
 
 
+3회차풀이
+
+ import java.util.*;
+
+
+class Main {
+
+
+
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+
+        String s = input.next(); // 문자열
+        char t = input.next().charAt(0); // 문자 (char로 받음)
+
+        // 정답을 저장할 배열 (문자열의 길이만큼)
+        int[] answer = new int[s.length()];
+
+        // 문자열 길이가 100을 넘지 않으므로 초기 거리는 무조건 100보다 큰 수로 설정
+        int count = 1000;
+
+        // 1. 왼쪽에서 오른쪽으로 탐색
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == t) {
+                count = 0; // 타겟 문자를 만나면 거리는 0
+            } else {
+                count++;   // 아니면 거리가 1씩 증가
+            }
+            answer[i] = count;
+        }
+
+        count = 1000; // 거리를 다시 큰 수로 초기화
+
+        // 2. 오른쪽에서 왼쪽으로 탐색
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == t) {
+                count = 0;
+            } else {
+                count++;
+                // 기존에 저장된 값(왼쪽에서 잰 거리)과
+                // 현재 값(오른쪽에서 잰 거리) 중 더 작은 값을 저장
+                answer[i] = Math.min(answer[i], count);
+            }
+        }
+
+
+        for (int i = 0; i < answer.length; i++) {
+            System.out.print(answer[i]);
+            if (i < answer.length - 1) {
+                System.out.print(" "); // 마지막 숫자가 아닐 때만 공백 출력
+            }
+        }
+    }
+
+}
+
+
+3회차 틀린 풀이
+
+ import java.util.*;
+
+
+class Main {
+
+
+
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+
+        String s = input.next(); //문자열
+        String t = input.next(); //문자
+
+        int count = 1;
+
+        List<Integer> list = new ArrayList<>();//정답 저장
+
+        String[] split = s.split("");
+
+        //왼쪽에서 시작
+        for (int i = 0; i < split.length; i++) {
+
+            if (split[i].equals(t)) {
+                list.add(0);
+                count = 1;
+            } else {
+                list.add(count);
+                count++;
+            }
+        }
+
+        count = 1;
+
+        //오른쪽에서 시작
+        for (int i = split.length - 1; i >= 0; i--) {
+
+            if (split[i].equals(t)) {
+                count = 1;
+            } else {
+
+                if (list.get(i) > count) {
+                    list.remove(i);
+                    list.add(i,count);
+                    count++;
+                }
+            }
+        }
+
+        for (Integer i : list) {
+            System.out.print(i + " ");
+        }
+
+    }
+
+}
+
+
 
 
 
