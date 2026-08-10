79c2f82e1801aa283016e68a4e5d23220d8e781a
xodnrok <103110967+xodnrok@users.noreply.github.com>
2026-08-10 15:42:02 +0900

    Update and rename 2-4 피보나치수(3회차).java to 2-4 피보나치수(3회차).java
    2회차 풀때 0번인덱스 1번인덱스에 1값을 넣고 시작하는것은 기억이 났고 반복문의 범위를 몇으로 설정할지 고민을했다. 그냥 n으로 해버리면 에러가 발생했고 그래서 1 1 2 이렇게 있을때 가운데 있는 1을 기준으로 즉 인덱스 1을 기준으로 반복문을 시작해서 돌렸는데 끝나는 범위를 정하기가 너무 애매했다.
    
    그래서 첫번째 1 즉 인덱스 0번부터 시작하는 반복문 시작범위를 지정하니 끝나는값도 깔금하게 되었다. 깔끔하게 n-2하니까 알맞게 떨어졌다.
    
    3회차 풀이 0번 , 1번에 시작값 1을 넣고 이후 풀었다.

--- 2-4 피보나치수(3회차).java
+++ 2-4 피보나치수(3회차).java
@@ -69,5 +69,38 @@ class Main {
     }
 }
 
+3회차 풀이
+
+import java.util.*;
+
+
+class Main {
+
+
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int count = input.nextInt();
+
+        int[] arr = new int[count];
+
+        arr[0] = 1;
+        arr[1] = 1;
+
+        for (int i = 2; i < count; i++) {
+            arr[i] = arr[i - 2] + arr[i - 1];
+        }
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+
+
+    }
+
+}
+
 
 
